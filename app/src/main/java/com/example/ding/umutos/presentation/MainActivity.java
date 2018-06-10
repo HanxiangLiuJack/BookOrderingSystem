53c48a6bf30dd153d56376d67b560a6f006be7cd
Ding-X <pengx345@myumanitoba.ca>
2018-06-10 09:21:00 -0500

    Presentation Folder created

--- app/src/main/java/com/example/ding/umutos/presentation/MainActivity.java
+++ app/src/main/java/com/example/ding/umutos/presentation/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.ding.umutos;
+package com.example.ding.umutos.presentation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.ding.umutos.R;
 
 public class MainActivity extends AppCompatActivity {
 

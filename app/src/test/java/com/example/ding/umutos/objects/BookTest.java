e760f06ad20764acbf7290ca267d092446fbd43a
Hanxiang Liu <liuh3428@myumanitoba.ca>
2018-06-11 15:58:24 -0500

    Update BookTest.java

--- app/src/test/java/com/example/ding/umutos/objects/BookTest.java
+++ app/src/test/java/com/example/ding/umutos/objects/BookTest.java
@@ -1,8 +1,9 @@
-package com.example.jackdeemo.myapplication;
+package com.example.ding.umutos;
 
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).

774901804d56fe78ae1637960a39697bc4c3e48e
Hanxiang Liu <liuh3428@myumanitoba.ca>
2018-07-02 18:27:15 -0500

    Update Account.java

--- app/src/main/java/com/example/ding/umutos/objects/Account.java
+++ app/src/main/java/com/example/ding/umutos/objects/Account.java
@@ -4,23 +4,27 @@ package com.example.ding.umutos.objects;
    Account object:
    1. userID, which is a final int cannot be changed or setted.
    2. userName, the user's name.
+   3. passWord.
 */
 
 public class Account
 {
     private final int userID;
     private String userName;
+    private String passWord;
    
-    public Account( int userID , String userName )
+    public Account( int userID , String userName ,String passWord)
     {
         this.userID = userID;
         this.userName = userName;
+        this.passWord = passWord;
     }
     
     public int getUserID()
     {
         return userID;
     }
+
     public String getUserName()
     {
         return userName;
@@ -30,4 +34,8 @@ public class Account
     {
         userName = newName;
     }
+
+    public void setPassWord(String p){passWord = p;}
+
+    public String getPassWord(){return passWord;}
 }

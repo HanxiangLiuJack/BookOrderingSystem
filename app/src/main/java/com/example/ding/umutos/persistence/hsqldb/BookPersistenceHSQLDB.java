80f61d6e0dedd9a209ea1afab5fbf0e7fb04acb9
MangoChili <374776276@qq.com>
2018-07-03 03:27:59 -0500

    Some changes in BookPersistenceHSQLDB, re-submit Exception file

--- app/src/main/java/com/example/ding/umutos/persistence/hsqldb/BookPersistenceHSQLDB.java
+++ app/src/main/java/com/example/ding/umutos/persistence/hsqldb/BookPersistenceHSQLDB.java
@@ -1,6 +1,40 @@
 package com.example.ding.umutos.persistence.hsqldb;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.ding.umutos.objects.Book;
 import com.example.ding.umutos.persistence.BookPersistence;
 
 public class BookPersistenceHSQLDB implements BookPersistence {
+
+    private final Connection c;
+
+    public BookPersistenceHSQLDB(final String dbPath) {
+        try {
+            this.c = DriverManager.getConnection("jdbc:hsqldb:file:" + dbPath, "SA", "");
+        } catch (final SQLException e) {
+            throw new PersistenceException(e);
+        }
+    }
+
+    private Book fromResultSet(final ResultSet rs) throws SQLException {
+        String BookName = rs.getString("BookName");
+        String authorName = rs.getString("authorName");
+        int bookPicture = rs.getInt("bookPicture");
+        String bookDescription = rs.getString("bookDescription");
+        String bookCategory = rs.getString("bookCategory");
+        double price = rs.getDouble("price");
+        int ownerID = rs.getInt("ownerID");
+
+        return new Book(BookName, authorName, bookPicture, bookDescription, bookCategory, price, ownerID);
+    }
+
 }

be74ff3df1164c7f85d7bfb89bd8fb6e93c8b726
Hanxiang Liu <liuh3428@myumanitoba.ca>
2018-07-04 00:23:36 -0500

    Upload AccessOrderTest, Not finished

---
+++ app/src/test/java/com/example/ding/umutos/business/AccessOrderTest.java
@@ -0,0 +1,39 @@
+package com.example.ding.umutos.business;
+
+import com.example.ding.umutos.objects.Order;
+import com.example.ding.umutos.persistence.OrderPersistence;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class AccessOrderTest {
+  private AccessOrders accessOrders;
+  private OrderPersistence orderPersistence;
+
+  @Before
+    public void setup()
+  {
+      orderPersistence = mock(OrderPersistence.class);
+      accessOrders = new AccessOrders(orderPersistence);
+  }
+
+
+  @After
+    public void tearDown(){ accessOrders = null ; orderPersistence = null ;}
+
+    @Test
+    public void testOrderHistory()
+    {
+        //
+    }
+}

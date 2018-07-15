588eb34ff1be6b69d91c5e9662c0b05091ab7178
yugu <guy3@myumanitoba.ca>
2018-07-15 01:49:56 -0500

    add object Wish

--- app/src/main/java/com/example/ding/umutos/objects/Wish.java
+++ app/src/main/java/com/example/ding/umutos/objects/Wish.java
@@ -1,4 +1,36 @@
 package com.example.ding.umutos.objects;
 
+
 public class Wish {
+    private int bookID;
+    private double price;
+    private String ownerName;
+    private String bookName;
+
+    public Wish(int bookID, double price, String ownerName, String bookName)
+    {
+        this.bookName = bookName;
+        this.price = price;
+        this.ownerName = ownerName;
+        this.bookID=bookID;
+    }
+
+    public void setPrice(double p)
+    {
+        this.price = p;
+    }
+
+    public void setBookID(int bookID)
+    {
+        this.bookID = bookID;
+    }
+
+    public void setName(String name)
+    {
+        this.bookName = name;
+    }
+
+    public void setOwnerName(String name){this.ownerName=name;}
+
+
 }

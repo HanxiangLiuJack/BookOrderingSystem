c8ad634bb2d4701b4c854015d74ff29b3ca6cc70
JacKDeemo <liuh3428@umanitoba.ca>
2018-07-16 12:25:30 -0500

    Merge branch 'GuYu' of https://code.cs.umanitoba.ca/comp3350-summer2018/iOS into HanxiangLiu
    # Conflicts:
    #	app/src/androidTest/java/com/example/ding/umutos/Acceptance/OrderHisitoryTest.java
    #	app/src/androidTest/java/com/example/ding/umutos/Acceptance/PriceUpDownTest.java
    #	app/src/androidTest/java/com/example/ding/umutos/Acceptance/ShoppingCartTest.java

--- app/src/androidTest/java/com/example/ding/umutos/Acceptance/OrderHistoryTest.java
+++ app/src/androidTest/java/com/example/ding/umutos/Acceptance/OrderHistoryTest.java
@@ -4,6 +4,12 @@ import com.example.ding.umutos.application.Service;
 import com.example.ding.umutos.objects.Account;
 import com.example.ding.umutos.presentation.HomeActivity;
 
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.action.CoordinatesProvider;
+import android.support.test.espresso.action.GeneralClickAction;
+import android.support.test.espresso.action.Press;
+import android.support.test.espresso.action.Tap;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
@@ -11,6 +17,7 @@ import android.support.test.runner.AndroidJUnit4;
 import com.example.ding.umutos.R;
 
 
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.Before;
@@ -27,6 +34,7 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.RootMatchers.isDialog;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
@@ -38,6 +46,10 @@ import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.core.AllOf.allOf;
 import static org.hamcrest.Matchers.is;
 
+import android.view.InputDevice;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RatingBar;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -46,9 +58,9 @@ public class OrderHistoryTest {
 
     @Rule
     public ActivityTestRule<LoginActivity> activityRule = new ActivityTestRule<>(LoginActivity.class);
-
-    @Test
-    public void OrderHistoryTest() {
+    @Before
+    public void setup()
+    {
         onView(withId(R.id.loginUserName)).perform(typeText("Xiao Peng"));
         closeSoftKeyboard();
 
@@ -61,6 +73,21 @@ public class OrderHistoryTest {
 
         closeSoftKeyboard();
 
+    }
+
+    @Test
+    public void OrderHistoryTest() {
+
+       testPlaceAnOrder();
+       testCreateAndEditAnOrder();
+       testConfirmAnOrder();
+       testViewOrderHistory();
+       testRateSeller();
+    }
+
+
+    private void testPlaceAnOrder()
+    {
         onData(anything()).inAdapterView(withId(R.id.cusListView)).atPosition(1).perform(click());
 
         onView(withId(R.id.singleBookAddToCart)).perform(click());
@@ -73,6 +100,10 @@ public class OrderHistoryTest {
 
         onView(withText("YES")).inRoot(isDialog()).check(matches(isDisplayed())).perform(click());
 
+    }
+
+    private void testCreateAndEditAnOrder()
+    {
         onView(withId(R.id.editFirstName)).perform(typeText("abcd"));
         closeSoftKeyboard();
 
@@ -93,14 +124,75 @@ public class OrderHistoryTest {
 
         onView(withId(R.id.addSubmit)).perform(click());
 
-        onView(withText("OK")).inRoot(isDialog()).check(matches(isDisplayed())).perform(click());
-
-        closeSoftKeyboard();
-
-        onView(withId(R.id.viewCustomerHistory)).perform(click());
 
 
     }
 
+    private void testConfirmAnOrder()
+    {
+        onView(withText("OK")).inRoot(isDialog()).check(matches(isDisplayed())).perform(click());
 
+        closeSoftKeyboard();
+    }
+
+    private void testViewOrderHistory()
+    {
+        onView(withId(R.id.viewCustomerHistory)).perform(click());
+
+    }
+
+    private void testRateSeller()
+    {
+        onData(anything()).inAdapterView(withId(R.id.historyBookList)).atPosition(0).perform(click());
+
+        onView(withId(R.id.editRatingBar)).perform(setRating(4.5f));
+
+        onView(withId(R.id.rateSubmit)).perform(click());
+
+    }
+
+    private static ViewAction setRating(final float rating) {
+        if (rating % 0.5 != 0) {
+            throw new IllegalArgumentException("Rating must be multiple of 0.5f");
+        }
+
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isAssignableFrom(RatingBar.class);
+            }
+
+            @Override
+            public String getDescription() {
+                return "Set rating on RatingBar in 0.5f increments";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                GeneralClickAction viewAction = new GeneralClickAction(
+                        Tap.SINGLE,
+                        new CoordinatesProvider() {
+                            @Override
+                            public float[] calculateCoordinates(View view) {
+                                int[] locationOnScreen = new int[2];
+
+                                view.getLocationOnScreen(locationOnScreen);
+                                int screenX = locationOnScreen[0];
+                                int screenY = locationOnScreen[1];
+                                int numStars = ((RatingBar) view).getNumStars();
+                                float widthPerStar = 1f * view.getWidth() / numStars;
+                                float percent = rating / numStars;
+                                float x = screenX + view.getWidth() * percent;
+                                float y = screenY + view.getHeight() * 0.5f;
+                                return new float[]{x - widthPerStar * 0.5f, y};
+                            }
+                        },
+                        Press.FINGER,
+                        InputDevice.SOURCE_UNKNOWN,
+                        MotionEvent.BUTTON_PRIMARY
+                );
+                viewAction.perform(uiController, view);
+            }
+        };
+    }
 }

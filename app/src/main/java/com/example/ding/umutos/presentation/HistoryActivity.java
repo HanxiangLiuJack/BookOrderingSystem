2a184f2466ae5e09a605ad8115c664503aac0e15
Ding-X <pengx345@myumanitoba.ca>
2018-07-15 22:31:40 -0500

    Rating done.

--- app/src/main/java/com/example/ding/umutos/presentation/HistoryActivity.java
+++ app/src/main/java/com/example/ding/umutos/presentation/HistoryActivity.java
@@ -3,28 +3,29 @@ package com.example.ding.umutos.presentation;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.example.ding.umutos.R;
 import com.example.ding.umutos.business.AccessAccounts;
-import com.example.ding.umutos.business.AccessBooks;
 import com.example.ding.umutos.business.AccessOrders;
-import com.example.ding.umutos.objects.Book;
 import com.example.ding.umutos.objects.Order;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+
 public class HistoryActivity extends AppCompatActivity {
     private ListView bookList;
     private AccessOrders accessOrderList;
     private List<Order> newOrderList;
-    int userType;
-    String userName;
+    private int userType;
+    private String userName, sellerName;
 
 
     @Override
@@ -42,6 +43,8 @@ public class HistoryActivity extends AppCompatActivity {
         else
             newOrderList=accessOrderList.sellerOrderHistory(userName);
         loadList(newOrderList);
+
+
 
         TextView historyBar=(TextView)findViewById(R.id.historyBar);
         historyBar.setText("Order history");
@@ -67,6 +70,25 @@ public class HistoryActivity extends AppCompatActivity {
                 new String[] {"account", "title", "price","address" },
                 new int[] { R.id.hisBookListByAccount, R.id.hisBookListTitle, R.id.hisBookListPrice , R.id.hisBookListAddress});
         bookList.setAdapter(sItems);
+
+        if (userType==1){
+
+            bookList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                @Override
+                public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                        long arg3) {
+                    HashMap<String,String> map=(HashMap<String,String>)bookList.getItemAtPosition(arg2);
+
+                    sellerName=map.get("account");
+                    Intent intent = new Intent(HistoryActivity.this,RateActivity.class);
+                    intent.putExtra("userName", userName);
+                    intent.putExtra("userType", userType);
+                    intent.putExtra("sellerName", sellerName);
+                    HistoryActivity.this.startActivity(intent);
+
+                }
+            });
+        }
     }
 
     public void buttonHistoryBack(View view) {

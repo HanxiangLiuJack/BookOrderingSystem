f3518fb5ce86c668130dc2dcb8740a70aacefa4d
yugu <guy3@myumanitoba.ca>
2018-07-16 13:09:03 -0500

    Merge branch 'TestAndLogic' of https://code.cs.umanitoba.ca/comp3350-summer2018/iOS into GuYu
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- app/src/androidTest/java/com/example/ding/umutos/AllAcceptanceTest.java
+++ app/src/androidTest/java/com/example/ding/umutos/AllAcceptanceTest.java
@@ -3,7 +3,7 @@ package com.example.ding.umutos;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import com.example.ding.umutos.business.integrationtests.*;
+//import com.example.ding.umutos.business.integrationtests.*;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
